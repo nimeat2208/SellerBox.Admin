578792b883a7ac5255fdefebf1aec5a6ab28eab7
tmkim <tmkim@VM0197>
2021-05-04 16:51:16 +0900

    초기 커밋.

---
+++ SellerBox.Admin/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SellerBox.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellerBox.Admin.Controllers
+{
+    [Route("Login")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        [Route("")] 
+        [Route("[action]")] 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("[action]")]  
+        public IActionResult Forgot()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("[action]")] 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("[action]")] 
+        public IActionResult Logout() { 
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
